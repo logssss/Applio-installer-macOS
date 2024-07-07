7c408ef017ed7a8d73af347ca0a826319082664e
desconocidoxxx <144498068+Anthonyxd22@users.noreply.github.com>
2024-07-07 16:17:59 -0400

    Update run-install.sh Update

--- run-install.sh
+++ run-install.sh
@@ -6,29 +6,21 @@ rm *.bat
 # Function to create or activate a virtual environment
 prepare_install() {
     if [ -d ".venv" ]; then
-        echo "Venv found. This implies Applio has been already installed or this is a broken install"
-        printf "Do you want to execute run-applio.sh? (Y/N): " >&2
-        read -r r
-        r=$(echo "$r" | tr '[:upper:]' '[:lower:]')
-        if [ "$r" = "y" ]; then
-            ./run-applio.sh && exit 1
-        else
-            echo "Ok! The installation will continue. Good luck!"
-        fi
+        echo "Venv found. This implies Applio has already been installed or this is a broken install."
         . .venv/bin/activate
     else
         echo "Creating venv..."
         requirements_file="requirements.txt"
-        echo "Checking if python exists"
+        echo "Checking if Python exists..."
         if command -v python3.10 > /dev/null 2>&1; then
             py=$(which python3.10)
-            echo "Using python3.10"
+            echo "Using Python 3.10"
         else
             if python --version | grep -qE "3\.(7|8|9|10)\."; then
                 py=$(which python)
-                echo "Using python"
+                echo "Using Python"
             else
-                echo "Please install Python3 or 3.10 manually."
+                echo "Please install Python 3 or 3.10 manually."
                 exit 1
             fi
         fi
@@ -66,22 +58,12 @@ finish() {
     fi
     clear
     echo "Applio has been successfully downloaded. Run the file run-applio.sh to run the web interface!"
+    echo "To re-install, please delete the existing '.venv' directory and run this script again."
+    echo "Giving execute permissions to run-applio.sh..."
+    chmod +x run-applio.sh
+    ./run-applio.sh
     exit 0
 }
 
-# Loop to the main menu
-if [ "$(uname)" = "Darwin" ]; then
-    if ! command -v brew >/dev/null 2>&1; then
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    else
-        brew install python@3.10
-        export PYTORCH_ENABLE_MPS_FALLBACK=1
-        export PYTORCH_MPS_HIGH_WATERMARK_RATIO=0.0
-    fi
-elif [ "$(uname)" != "Linux" ]; then
-    echo "Unsupported operating system. Are you using Windows...?"
-    echo "If yes, use the batch (.bat) file instead of this one!"
-    exit 1
-fi
-
+# Main script execution begins here
 prepare_install
